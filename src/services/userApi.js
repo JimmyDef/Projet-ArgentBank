2b1b11fdfac9c90ce6c26cea9c579824d2efaa4c
Jimmy Defains <jimmydef@outlook.fr>
2024-02-27 07:17:22 +0100

    Init.

---
+++ src/services/userApi.js
@@ -0,0 +1,6 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/api/v1/user" }),
+});
